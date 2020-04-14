4e12e0c9d37711b3843dcc08f18e3e1c4f5948fd
Shivani Dubey <shivani111296cse@gmail.com>
2020-04-14 16:24:20 +0530

    added topic to push data to corehr

--- kafka-consumer.js
+++ kafka-consumer.js
@@ -17,6 +17,7 @@ kafkaLogging.setLoggerProvider(consoleLoggerProvider);
 const kafka = require('kafka-node');
 var ConsumerGroup = kafka.ConsumerGroup;
 
+
 var options = {
     kafkaHost: 'localhost:9092', // connect directly to kafka broker (instantiates a KafkaClient)
     batch: undefined, // put client batch settings if you need them
@@ -39,7 +40,44 @@ var options = {
     onRebalance: (isAlreadyMember, callback) => { callback(); } // or null
 };
 
+var options1 = {
+    kafkaHost: 'localhost:9092', // connect directly to kafka broker (instantiates a KafkaClient)
+    batch: undefined, // put client batch settings if you need them
+    ssl: true, // optional (defaults to false) or tls options hash
+    groupId: 'kafka-chr',
+    sessionTimeout: 15000,
+    // An array of partition assignment protocols ordered by preference.
+    // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
+    protocol: ['roundrobin'],
+    encoding: 'utf8', // default is utf8, use 'buffer' for binary data
+
+    // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
+    // equivalent to Java client's auto.offset.reset
+    fromOffset: 'earliest', // default
+    commitOffsetsOnFirstJoin: true, // on the very first time this consumer group subscribes to a topic, record the offset returned in fromOffset (latest/earliest)
+    // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
+    outOfRangeOffset: 'earliest', // default
+    // Callback to allow consumers with autoCommit false a chance to commit before a rebalance finishes
+    // isAlreadyMember will be false on the first connection, and true on rebalances triggered after that
+    onRebalance: (isAlreadyMember, callback) => { callback(); } // or null
+};
+
 var consumerGroup = new ConsumerGroup(options, 'poc-topic');
+
+var coreHrConsumerGroup = new ConsumerGroup(options1, 'data-to-corehr');
+
+coreHrConsumerGroup.on('message', function (message) {
+    console.log(message);
+});
+
+coreHrConsumerGroup.on('error', function (error) {
+    console.log(error);
+});
+
+
+coreHrConsumerGroup.on('connect', function () {
+    console.log('connected')
+});
 
 consumerGroup.on('message', function (message) {
     console.log(message);
